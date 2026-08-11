348b7a4b072aca632c9cddfc6511f986067ebc68
tomar <tomarankit78@gmail.com>
2026-08-11 19:09:49 +0530

    Initial commit and Project Setup

---
+++ jwtAuthentication/src/test/java/org/example/jwtauthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
